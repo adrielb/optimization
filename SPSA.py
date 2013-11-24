ef6f42df31cc8315ac536ab4513016ffd3a09849
Adriel Bergmann <adrielb@users.noreply.github.com>
2013-11-24 14:17:23 -0500

    dQnew

--- SPSA.py
+++ SPSA.py
@@ -4,6 +4,9 @@ Simultaneous Perturbation Stochastic Approximation
 import numpy as np
 
 def delta_gen( p ):
+    '''
+    returns {-1, 1}
+    '''
     return 2 * np.random.randint(low=0, high=2, size=p ) - 1
 
 def SPSAk( ):
@@ -14,8 +17,9 @@ def SPSAk( ):
     grad = (L1 - L2) / ( 2*c*d )
 
     g =  w
+    dQnew = -a* g
 
-    Qnew = Qk - a * g
+    Qnew = Qk + dQ
     Qnew = constraints( Qnew )
     Lnew = loss( Qnew )
 
